ab58b3dca6c9d520600577400892391f9cc32ccc
Jyoti-SHM <jyotihospitalityminds@gmail.com>
2025-02-20 16:13:55 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,104 @@
+<!--================ Footer ================-->
+    <footer id="mad-footer" class="mad-footer">
+      <!--================ Footer row ================-->
+      <div class="mad-footer-main">
+        <div class="container-fluid">
+          <div class="row justify-content-between vr-size-2">
+           <div class="col-xl-4 col-lg-4 col-md-6">
+              <!--================ Widget ================-->
+              <section class="mad-widget">
+               <h6 class="mad-widget-title white">About Us</h6>
+                <p class="content-element-4 white" align="justify">Welcome to Hotel Mussoorie Heights, this hotel is the best hotel in Mussoorie. It offers quality services with amazing view at an affordable price.</p>
+                <div class="mad-social-icons">
+                  <ul>
+                    <li>
+                      <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fab fa-instagram"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fab fa-twitter"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fab fa-tripadvisor"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><div class="messenger">&nbsp;</div></a>
+                    </li>
+                  </ul>
+                </div>
+              </section>
+              <!--================ End of Widget ================-->
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <!--================ Widget ================-->
+              <section class="mad-widget">
+              <h6 class="mad-widget-title white">Our Location</h6>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3439.493951511578!2d78.08428351512475!3d30.450444381736343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7286f43c87b59b6c!2zMzDCsDI3JzAxLjYiTiA3OMKwMDUnMTEuMyJF!5e0!3m2!1sen!2sin!4v1649942100649!5m2!1sen!2sin" width="100%" height="180" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+              </section>
+              <!--================ End of Widget ================-->
+            </div>
+            <div class="col-xl-2 col-lg-4 col-md-6">
+              <!--================ Widget ================-->
+              <section class="mad-widget">
+                <h6 class="mad-widget-title white">Menu</h6>
+                <div class="mad-vr-list menu">
+                  <ul>
+                    <li>
+                      <a href="exclusive-super-deluxe-room.php" class="mad-text-link"> 
+                          <span>Our Rooms</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="services.php" class="mad-text-link">
+                          <span>Our Services</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="gallery.php" class="mad-text-link">
+                          <span>Photo Gallery</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="contact.php" class="mad-text-link">
+                          <span>Contact Us</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="around-mussoorie.php" class="mad-text-link">
+                          <span>Around Mussoorie</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </section>
+              <!--================ End of Widget ================-->
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <!--================ Widget ================-->
+              <section class="mad-widget">
+                <h6 class="mad-widget-title white">Contact Us</h6>
+                <div class="mad-vr-list">
+                  <ul class="white">
+                    <li><i class="fa fa-map-marker white" aria-hidden="true"></i>&nbsp;<a href="https://maps.app.goo.gl/vUWUh8MrtHrXM5vJ6"> Hotel Mussoorie Heights, Picture Palace - Kincraig Road, Tara Hall, Mussoorie - 248179, Uttarakhand</a></li>
+                    <li><i class="fa fa-phone white" aria-hidden="true"></i>&nbsp;<a href="tel:+919897055949"> 9897055949</a> ,<a href="tel:+919897055999">9897055999</a>, <a href="tel:+9101352633300">01352633300</a>,  <a href="tel:+9101352633399">01352633399</a></li>
+                    <li><i class="fa fa-envelope white" aria-hidden="true"></i>&nbsp; <a href="mailto:info@mussoorieheights.com">info@mussoorieheights.com</a></li>
+                  </ul>
+                </div>
+              </section>
+              <!--================ End of Widget ================-->
+            </div>
+          </div>
+          <div class="mad-footer-bottom">
+            <p class="copyrights align-center white"> Copyright © 2021. Designed By <a href="http://webcitysolutions.in/" target="_blank">Webcity Solutions</a>. All Rights Reserved. </p>
+          </div>
+        </div>
+      </div>
+      <!--================ End of Footer row ================-->
+    </footer>
+    <!--================ End of Footer ================-->
+
+    <div class="callnow" ><a href="tel:+91-9897055999"><img src="images/call.png" width="50"></a></div>
+
+<div class="whatsapps"><a href="https://wa.me/+919897055999?text=Hi%20Can%20We%20Have%20a%20Chat?" target="_blank"><img src="images/whatsapp-icon.png" width="50"></a></div>
---
+++ include/header.php
@@ -0,0 +1,82 @@
+ <!-----top header------->
+   <div class="mad-header">
+   <div class="container-fluid top-header">
+   <div class="row">
+   <div class="col-lg-12 display-f">
+   <div class="col-lg-6 col-xs-12">
+   <span class="mobile-hide"><i class="fa fa-phone"></i>&nbsp;01352633300,
+01352633399&nbsp;&nbsp;&nbsp;&nbsp;</span>
+   <span class="mobile-hide"><i class="fa fa-envelope"></i>&nbsp;info@mussoorieheights.com</span>
+   </div>
+   <div class="col-lg-6 col-xs-12 social-icon mobile-center" align="right">
+   <a href="https://goo.gl/maps/igUm6DjgxsAJQHsX8" title="Location" target="_blank"><i class="fa fa-map-marker" aria-hiddwn="true"></i></a>
+   <a href="#" title="Facebook"><i class="fa fa-facebook" aria-hiddwn="true"></i></a>
+   <a href="#" title="Instagram"><i class="fa fa-instagram" aria-hiddwn="true"></i></a>
+   <a href="#" title="Twitter"><i class="fa fa-twitter" aria-hiddwn="true"></i></a>
+   <a href="#" title="Trip Advisor"><i class="fa fa-tripadvisor" aria-hiddwn="true"></i></a>
+   </div>
+   </div>
+   </div>
+   </div>
+   </div>
+  <!-----top header end----->
+  
+    <!--================ Header ================-->
+    <header id="mad-header" class="mad-header">
+      <div class="mad-header-section--sticky-xl">
+        <div class="container-fluid">
+          <div class="mad-header-items">
+            <div class="mad-header-item">
+              <a href="index.php" class="mad-logo desktop-logo"><img src="images/logo.png" alt="Logo" title="Logo"/></a>
+               <a href="index.php" class="mad-logo mobile-logo"><img src="images/mobile-logo.png" alt="Logo" title="Logo"/></a>
+            </div>
+            <div class="mad-header-item">
+              <!--================ Navigation ================-->
+              <nav class="mad-navigation-container">
+                <ul class="mad-navigation mad-navigation--vertical-sm">
+                  <li class="menu-item">
+                    <a href="index.php">Home</a>
+                  </li>
+                  <li class="menu-item menu-item-has-children">
+                    <a href="#">Rooms</a>
+                    <!--================ Sub Menu ================-->
+                    <ul class="sub-menu">
+                      <li class="menu-item">
+                        <a href="exclusive-super-deluxe-room.php">Exclusive Super Deluxe Rooms</a>
+                      </li>
+                      <li class="menu-item">
+                        <a href="deluxe-room.php">Deluxe Rooms</a>
+                      </li>
+                    </ul>
+                    <!--================ End of Sub Menu ================-->
+                  </li>
+                  <li class="menu-item">
+                    <a href="services.php">Our Services</a>
+                  </li>
+                  <li class="menu-item">
+                    <a href="gallery.php">Picture Gallery</a>
+                  </li>
+                  <li class="menu-item">
+                    <a href="around-mussoorie.php">Around Mussoorie</a>
+                  </li>
+                  <li class="menu-item">
+                    <a href="blog.php">Blogs</a>
+                  </li>
+                  <li class="menu-item">
+                    <a href="contact.php">Contact Us</a>
+                  </li>
+                </ul>
+              </nav>
+              
+              <!--================ End of Navigation ================-->
+              <div class="mad-actions">
+                <div class="mad-col">
+                  <a href="contact.php" class="btn">Book Now</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!--================ End of Header ================-->
